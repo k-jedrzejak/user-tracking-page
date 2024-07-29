c62a687a2733c55f62484028d58ed582cf066aee
k-jedrzejak <kjedrzejak91@gmail.com>
2024-07-29 15:12:15 +0200

    save user in session

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ import express from 'express';
 import path from 'path';
 import { connect } from 'mongoose';
 import { fileURLToPath } from 'url';
+import User from './models/user.js';
+import AccessLog from './models/accessLog.js';
+import session from 'express-session';
+import fetchUserData from './utils/fetchUserData.js';
+import MongoStore from 'connect-mongo';
 
 dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
@@ -15,16 +20,50 @@ const URI = process.env.MONGODB_URI;
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'));
 
-try {
-  connect(URI);
-  console.log("MongoDB connection successful.", URI);
-} catch (err) {
-  console.error("Failed to connect to MongoDB:", err);
-}
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render("index");
-})
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'secret-key',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: URI })
+  })
+);
+
+app.use(async (req, res, next) => {
+  if (!req.session.userId) {
+    const userData = await fetchUserData();
+    if (userData) {
+      const newUser = new User(userData);
+      await newUser.save(); 
+      req.session.userId = newUser._id; 
+      const newAccessLog = new AccessLog({ userId: newUser._id });
+      await newAccessLog.save(); 
+    }
+  }
+  next(); 
+});
+
+
+
+
+
+app.get("/", async (req, res) => {
+  const user = await User.findById(req.session.userId);
+  res.render("index", { user });
+});
+
+// connect to MongoDB and run the server
+connect(URI)
+.then(() => {
+  console.log("MongoDB connection successful.", URI);
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log('Server is running on port 3000');
+  });
 })
+.catch(err => {
+  console.error('Failed to connect to MongoDB', err);
+});
+
+
