78697ce6f3e4b6c43356856bfd948e8079bb154a
k-jedrzejak <kjedrzejak91@gmail.com>
2024-08-01 07:27:22 +0200

    changed nginx

--- server/server.js
+++ server/server.js
@@ -10,9 +10,9 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(cors({
-//   origin: 'http://localhost:8000',
-// }));
+app.use(cors({
+  origin: 'http://localhost:8000',
+}));
 
 app.use(express.json());
 app.use(sessionConfig); 
